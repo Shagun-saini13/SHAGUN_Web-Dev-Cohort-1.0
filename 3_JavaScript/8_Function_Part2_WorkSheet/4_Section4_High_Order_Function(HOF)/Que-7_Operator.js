75673863b3a5453983336c004c04ef2c331879ff
Shagun-saini13 <sainishagun001@gmail.com>
2025-05-24 14:35:50 +0530

    added

--- 3_JavaScript/8_Function_Part2_WorkSheet/4_Section4_High_Order_Function(HOF)/Que-7_Operator.js
+++ 3_JavaScript/8_Function_Part2_WorkSheet/4_Section4_High_Order_Function(HOF)/Que-7_Operator.js
@@ -1,10 +1,12 @@
 /*Q7. Create a function called operate that takes two numbers and a function. It should
 return the result of the function applied to the numbers*/
 
-function Operate(a,b,Multiply){
-    return multiply(a,b);
+function Operate(a, b, operation) {
+  return operation(a, b);
 }
-function multiply(a,b){
-    return a*b;
+
+function multiply(a, b) {
+  return a * b;
 }
-console.log(Operate(7,7));
+
+console.log(Operate(7, 7, multiply));
