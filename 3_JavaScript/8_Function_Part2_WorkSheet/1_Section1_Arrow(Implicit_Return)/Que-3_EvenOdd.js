9cbd8a9d393be09e326fa65b6aea777a20aded7e
Shagun-saini13 <sainishagun001@gmail.com>
2025-05-24 14:31:15 +0530

    added

--- 3_JavaScript/8_Function_Part2_WorkSheet/1_Section1_Arrow(Implicit_Return)/Que-3_EvenOdd.js
+++ 3_JavaScript/8_Function_Part2_WorkSheet/1_Section1_Arrow(Implicit_Return)/Que-3_EvenOdd.js
@@ -1,7 +1,7 @@
 /**Q3. Create a one-liner arrow function that returns "Even" if a number is divisible by 2,
 else returns "Odd".*/
 let EvenOdd=num=>{
-    if(num%2===0) return `${num} is Even`;
-    else return `${num} is Odd`;
+    if(num%2===0) return `${num} is Even Number`;
+    else return `${num} is Odd Number`;
 }
-console.log(EvenOdd(6));
+console.log(EvenOdd(7));
