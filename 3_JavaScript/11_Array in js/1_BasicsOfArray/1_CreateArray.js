8b480a181eca95094bbefe8bca3856581dafcaa5
Shagun-saini13 <sainishagun001@gmail.com>
2025-05-28 10:17:42 +0530

    added

--- 3_JavaScript/11_Array in js/1_BasicsOfArray/1_CreateArray.js
+++ 3_JavaScript/11_Array in js/1_BasicsOfArray/1_CreateArray.js
@@ -9,13 +9,3 @@ let arr1 = [2,4,"hlo", fun(3),{task1: 'exercise'}, 5];
 console.log(arr1);
 
 
-
-
-// let arr=[2,3];
-// arr.shift(3);
-// console.log(arr)
-// function add (arr){
-//     return arr[(arr.length-1)] + arr[0];
-// }
-// console.log(add([1,2,3,4,5]))
-
