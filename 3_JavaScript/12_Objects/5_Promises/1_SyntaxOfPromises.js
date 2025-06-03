4282247924fc5b1d12dadb0c418e984f9ce70980
Shagun-saini13 <sainishagun001@gmail.com>
2025-06-03 23:41:31 +0530

    added

--- 3_JavaScript/12_Objects/5_Promises/1_SyntaxOfPromises.js
+++ 3_JavaScript/12_Objects/5_Promises/1_SyntaxOfPromises.js
@@ -12,7 +12,7 @@ console.log(mypromise);
 
 //pending
 let hlo = new Promise (function(resolve,reject){
-    console.log("Pending");
+    console.log("Pending")
 })
 console.log(hlo);
 
@@ -21,7 +21,7 @@ let byee = new Promise (function(resolve,reject){
     console.log("reject")
     reject("failed")
 })
-console.log(byee);
+console.log(byee)
 //UnhandledPromiseRejection: This error originated either by throwing inside of an async function without a catch block, or by rejecting a promise which was not handled with .catch(). The promise rejected with the reason "failed"
 
 //That error means your code created a rejected Promise, but you didn’t handle the rejection properly using .catch() or a try...catch block (if using async/await)
